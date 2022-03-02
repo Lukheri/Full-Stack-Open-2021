f8fd91049bb51bee88fc76198f1d094fa691215d
Lukheri <luke.contrivida@gmail.com>
2022-03-02 22:31:29 +0800

    1.6
    exercise 1.6 submission

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Feedbacks = ({ feedbacks }) => (
+  <div>
+    <Button handleClick={feedbacks.goodFeedback.increase} text={feedbacks.goodFeedback.text} />
+    <Button handleClick={feedbacks.neutralFeedback.increase} text={feedbacks.neutralFeedback.text} />
+    <Button handleClick={feedbacks.badFeedback.increase} text={feedbacks.badFeedback.text} />
+  </div>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const feedbacks = {
+    goodFeedback: {
+      value:good,
+      text:"good",
+      increase: () => setGood(good+1)
+    },
+    neutralFeedback: {
+      value:neutral,
+      text:"neutral",
+      increase: () => setNeutral(neutral+1)
+    },
+    badFeedback: {
+      value:bad,
+      text:"bad",
+      increase: () => setBad(bad+1)
+    }
+  }
+
+  const Display = props => <div>{props.text} {props.value}</div>
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Feedbacks feedbacks={feedbacks} />
+      <h1>statistics</h1>
+      <Display text={feedbacks.goodFeedback.text} value={good} />
+      <Display text={feedbacks.neutralFeedback.text} value={neutral} />
+      <Display text={feedbacks.badFeedback.text} value={bad} />
+      
+    </div>
+  )
+}
+
+export default App
