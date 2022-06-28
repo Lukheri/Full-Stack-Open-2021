e63834000e778207db7e5e8d9725c4f250c522e1
Lukheri <luke.contrivida@gmail.com>
2022-06-28 11:14:42 +0800

    4.5
    answered exercise 4.5

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -1,5 +1,71 @@
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const listBlog = [
+  {
+    title: 'luke blog lmao',
+    author: 'Luke',
+    url: 'www.luke.com/blog',
+    likes: 5,
+    id: '62b9623fc203e66ef987ff53'
+  },
+  {
+    title: 'luke blog 2 hakhak',
+    author: 'Luke Heri',
+    url: 'www.luke.com/blog/hakhak',
+    likes: 6,
+    id: '62b968614e77eb1183646db7'
+  },
+  {
+    title: 'luke blog 3 hilak',
+    author: 'Luke Heri',
+    url: 'www.luke.com/blog/cry',
+    likes: 999,
+    id: '62b977d1f4a2bce86494d524'
+  },
+  {
+    title: 'luke blog 4 final',
+    author: 'Luke Heri',
+    url: 'www.luke.com/blog/YESSSSS',
+    likes: 1,
+    id: '62b979180a0d20fb704ab892'
+  }
+]
+
+const listBlog2 = [
+  {
+    title: 'luke blog lmao',
+    author: 'Luke',
+    url: 'www.luke.com/blog',
+    likes: 5,
+    id: '62b9623fc203e66ef987ff53'
+  },
+  {
+    title: 'luke blog 2.5 idk',
+    author: 'Luke Heriiiii',
+    url: 'www.luke.com/blog/??',
+    likes: 999,
+    id: '62b979180a0d20fb704ab892'
+  },
+  {
+    title: 'luke blog 3 hilak',
+    author: 'Luke Heri',
+    url: 'www.luke.com/blog/cry',
+    likes: 999,
+    id: '62b977d1f4a2bce86494d524'
+  }
+]
+  
 test('dummy returns one', () => {
   const blogs = []
 
@@ -7,49 +73,7 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const listBlog = [
-    {
-      title: 'luke blog lmao',
-      author: 'Luke',
-      url: 'www.luke.com/blog',
-      likes: 5,
-      id: '62b9623fc203e66ef987ff53'
-    },
-    {
-      title: 'luke blog 2 hakhak',
-      author: 'Luke Heri',
-      url: 'www.luke.com/blog/hakhak',
-      likes: 6,
-      id: '62b968614e77eb1183646db7'
-    },
-    {
-      title: 'luke blog 3 hilak',
-      author: 'Luke Heri',
-      url: 'www.luke.com/blog/cry',
-      likes: 999,
-      id: '62b977d1f4a2bce86494d524'
-    },
-    {
-      title: 'luke blog 4 final',
-      author: 'Luke Heri',
-      url: 'www.luke.com/blog/YESSSSS',
-      likes: 1,
-      id: '62b979180a0d20fb704ab892'
-    }
-  ]
-    
+describe('total likes', () => { 
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
@@ -57,5 +81,23 @@ describe('total likes', () => {
 
   test('list with multiple blogs', () => {
     expect(listHelper.totalLikes(listBlog)).toBe(1011)
+  })
+})
+
+describe('favorite blog', () => {
+  test('list with multiple blogs', () => {
+    expect(listHelper.favBlog(listBlog)).toEqual({
+      title: 'luke blog 3 hilak',
+      author: 'Luke Heri',
+      likes: 999,
+    })
+  })
+
+  test('list with same max likes', () => {
+    expect(listHelper.favBlog(listBlog2)).toEqual({
+      title: 'luke blog 2.5 idk',
+      author: 'Luke Heriiiii',
+      likes: 999
+    })
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,8 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favBlog = (blogs) => {
+  const likesList = blogs.map(blog => blog.likes)
+  const max = Math.max(...likesList)
+  const fav = blogs[likesList.indexOf(max)]
+  return {
+    'title': fav.title,
+    'author': fav.author,
+    'likes': fav.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favBlog
 }
 
