09b4b68d1b6f98ed3e2e2659cce24553459acdcd
Lukheri <luke.contrivida@gmail.com>
2022-06-29 17:31:21 +0800

    4.10
    answered exercise 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -78,6 +78,30 @@ test('check for id', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'Luke Heri',
+    url: 'http://coding.com/is/fun.html',
+    likes: 0
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const response = await api.get('/api/blogs')
+  
+  const contents = response.body.map(r => r.title)
+  
+  expect(response.body).toHaveLength(blogs.length + 1)
+  expect(contents).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
